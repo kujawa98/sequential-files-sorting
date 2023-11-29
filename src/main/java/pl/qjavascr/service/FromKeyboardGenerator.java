b400dae6e60a0fd5e0d5ff67f780873ba6ef208c
Kacper Kujawa <kacper.kujawa98@gmail.com>
2023-11-29 16:59:58 +0100

    Add generation from keyboard

--- src/main/java/pl/qjavascr/service/FromKeyboardGenerator.java
+++ src/main/java/pl/qjavascr/service/FromKeyboardGenerator.java
@@ -1,7 +1,28 @@
 package pl.qjavascr.service;
 
+import pl.qjavascr.model.Record;
+import pl.qjavascr.model.WritingTape;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static pl.qjavascr.util.ConstantsUtils.RECORD_LEN;
+
 public class FromKeyboardGenerator {
-    public static String readFromKeyboard(int howManyRecords) {
-        return "";
+    private static final String OUTPUT = "src/main/resources/output.dat";
+
+    public static void readFromKeyboard(int howManyRecords) throws FileNotFoundException {
+        WritingTape toTape = new WritingTape(OUTPUT);
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < howManyRecords; i++) {
+            System.out.print("Enter record content: ");
+            String record = input.nextLine();
+            if (record.length() > RECORD_LEN) {
+                System.out.println("Illegal record length - " + record.length() + " instead of " + RECORD_LEN);
+                continue;
+            }
+            toTape.writeRecord(new Record(record));
+        }
+        toTape.close();
     }
 }
